e93ac6bb7c85e8f166ed7ee005147a28d87de6d2
Advait <advait@advaits-macbook-pro.local>
2017-06-27 21:27:08 +0530

    Version 1.0 Basic

--- Log In Module Basic/ViewController.swift
+++ Log In Module Basic/ViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var mylabel: UILabel!
+    @IBOutlet weak var mytext: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        
+        do{
+            let results = try context.fetch(request)
+                for result in results as! [NSManagedObject]{
+      
+                    if let username = result.value(forKey: "name")as? String{
+                        mytext.alpha = 0
+                       logInButton.alpha = 0
+                        mylabel.text = "Hello "+username+"!"
+                    }
+                
+                }
+            }
+        catch{
+            print("Failed to fetch data")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
     }
+    @IBAction func logInButtonPressed(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let newValue = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+        
+        newValue.setValue(mytext.text!, forKey: "name")
+        do{
+            try context.save()
+            mytext.alpha = 0
+            logInButton.alpha = 0
+            mylabel.text = "Hello "+mytext.text!+"!"
+        }
+        catch{
+            print("Failed to save data")
+        }
 
+  
 
+    }
 }
-
